e79935d29cb566e3ff21eff47cbe48ed165221c6
Zen <zhengxu5511@gmail.com>
2014-04-22 01:35:49 +0800

    _ReverseWordsInString

--- ReverseWordsInString.cpp
+++ ReverseWordsInString.cpp
@@ -1,3 +1,4 @@
+//by zen 2014.04.22
 #include <stdio.h>
 #include <string>
 #include <iostream>
@@ -6,31 +7,42 @@ using namespace std;
 
 void reverseWords(string &s)
 {
-        vector<string> sum;
-        string str;
-        int i = 0;
-        int n =s.size();
-        while(s[i]!='\0'&&i<=n){
-            while(s[i]==' ')
+    if(s=="")
+        return;
+    vector<string> sum;
+    string str;
+    int i = 0;
+    int n =s.size();
+    while(i<n){
+        while(s[i]==' '&&i<n)
             i++;
-            while(s[i]!=' '&&s[i]!='\0')
-            {
-                str+=s[i];
-                i++;
-            }
-            sum.push_back(str);
-            str="";
+        if(i>=n)
+            break;
+        while(s[i]!=' '&&s[i]!='\0')
+        {
+            str+=s[i];
+            i++;
         }
-        s="";
+        sum.push_back(str);
+        cout<<str<<"kk"<<endl;
+        str="";
+    }
+    s="";
+    if(sum.size()>0)
+    {
+        
         for (vector<int>::size_type ix = sum.size()-1; ix > 0; --ix){
             s+=sum[ix];
             s+=' ';
         }
         s+=sum[0];
+    }
+    
+    
 }
 int main(int argc, char const *argv[])
 {
-	string s = "the sky is blue";
+	string s = " ";
 	reverseWords(s);
 	printf("The Result is:\n");
 	cout<<s<<endl;
