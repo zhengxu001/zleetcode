2c3a2a0fefc76904446d51a88fbac09c18931489
Zen <zhengxu5511@gmail.com>
2014-06-15 23:42:51 +0800

    0615leetcode

---
+++ PalindromePartitioningII.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCut(string s) {
+        int len = s.size();
+        int* dp = new int[len+1];
+        for(int i=len; i>=0; i--)
+            dp[i] = len-i;
+        bool** matrix = new bool*[len];
+        for(int i=0; i<len; i++)
+        {
+            matrix[i] = new bool[len];
+            memset(matrix[i], false, sizeof(bool)*len);
+        }
+        for(int i=len-1; i>=0; i--)
+            for(int j=i; j<len; j++)
+            {
+                if(s[i] == s[j] && (j-i<2 || matrix[i+1][j-1]))
+                {
+                    matrix[i][j] = true;
+                    dp[i] = min(dp[i], dp[j+1]+1);
+                }
+            }
+        return dp[0]-1;
+    }
+};
