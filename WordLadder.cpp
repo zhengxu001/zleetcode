7e543d95bcce35a4ca5d143fafeb23fcfd894b17
Zen <zhengxu5511@gmail.com>
2014-07-03 15:58:58 +0800

    leetcode0703

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -1,31 +1,47 @@
-public int ladderLength(String start, String HashSet<String> dict){
-    if (start.equals(end)) 
-	return 0;
-    if (isOneWordDiff(start, end))
-        return 2;
-    Queue<String> queue=new LinkedList<String>();
-    HashMap<String,Integer> dist=new HashMap<String,Integer>();
-    queue.add(start);
-    dist.put(start, 1);
-    while(!queue.isEmpty())
-    {
-        String head=queue.poll();
-        int headDist=dist.get(head);
-        for(int i=0;i<head.length();i++)
-        {
-            for(char j='a';j<'z';j++)
-            {
-                if(head.charAt(i)==j) continue;
-                StringBuilder sb=new StringBuilder(head);
-                sb.setCharAt(i, j);
-                if(sb.toString().equals(end)) return headDist+1;
-                if(dict.contains(sb.toString())&&!dist.containsKey(sb.toString()))
-                {
-                    queue.add(sb.toString());
-                    dist.put(sb.toString(), headDist+1);
+class Solution {
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(start.size()!=end.size()) return 0;
+        if(start == end) return 2;
+        
+        int min_value = 0;
+        unordered_set<string> unique;
+        unique.insert(start);
+        
+        queue<string> que;
+        que.push(start);
+        int q1=1;
+        int q2=0;
+        
+        while(q1>0) {
+            string s = que.front();
+            que.pop();
+            --q1;
+            
+            for(int i=0; i<s.size(); i++) {
+                string temp = s;
+                for(char c='a'; c<='z'; c++) {
+                    temp[i] = c;
+                    if(dict.find(temp)!=dict.end() && unique.find(temp)==dict.end()) {
+                        if(temp == end) {
+                            return min_value+2;
+                        } else {
+                            unique.insert(temp);
+                            que.push(temp);
+                            ++q2;
+                        }
+                    }
                 }
             }
+            
+            if(q1==0) {
+                q1 = q2;
+                q2 = 0;
+                ++min_value;
+            }
         }
+        return 0;
     }
-    return 0;
-}
+};
